a6cea3bffbc57490ea9769f710ebcfe0aa92a696
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2025-09-05 17:57:52 +0300

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,6 +0,0 @@
-// === Config ===
-// Если укажешь WS_URL — будет онлайн синхронизация и чат между разными устройствами.
-// Пример: const WS_URL = "wss://your-domain.com/ws";
-const WS_URL = "wss://kino-fap1.onrender.com/ws";
-// (Опционально) ключ для неофициального API Кинопоиска
-const KINOPOISK_API_KEY = "REPLACE_WITH_YOUR_KEY";
